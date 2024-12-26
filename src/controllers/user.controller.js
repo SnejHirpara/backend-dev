33f4a2a3494da23f7f8629d76000c81549227bb4
Snej Hirpara <snejhirpara123@gmail.com>
2024-12-27 01:21:06 +0530

    Add Get Watch History Endpoints

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
 import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -198,5 +199,285 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     }
 });
 
-export { loginUser, logoutUser, refreshAccessToken, registerUser };
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+    // get old & new password from req.body
+    // check old & new password empty
+    // verify old password in req with the password in user object from db
+    // check for incorrect old password
+    // update new password
+
+    const { oldPassword, newPassword } = req.body;
+
+    if (!oldPassword || !newPassword) {
+        throw new ApiError(400, "Old and new password are required");
+    }
+
+    try {
+        const user = await User.findById(req.user?._id);
+
+        const isValidOldPassword = await user.isPasswordCorrect(oldPassword);
+
+        if (!isValidOldPassword) {
+            throw new ApiError(401, "Invalid old password");
+        }
+
+
+        user.password = newPassword;
+        await user.save({ validateBeforeSave: false });
+
+        /**
+         * Below commented code doesn't required because we have writter .pre method in user.model.js
+         * and we have put "save" inside iit so it runs when we save the user. So, here at line no.: 226 we are saving user so it will automatically runs and encrypts the password if it is modified and in this case it is modified (newPassword)
+        */
+
+        // const encryptedNewPassword = await bcrypt.hash(newPassword, 10);
+        // await User.findByIdAndUpdate(
+        //     user._id,
+        //     {
+        //         $set: {
+        //             password: encryptedNewPassword
+        //         }
+        //     }
+        // );
+
+        return res.status(200).json(
+            new ApiResponse(200, {}, "Password changed successfully")
+        );
+    } catch (error) {
+        throw new ApiError(401);
+    }
+});
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+    return res.status(200).json(
+        new ApiResponse(200, req.user, "Current user fetched successfully")
+    );
+});
+
+const updateAccountDetails = asyncHandler(async (req, res) => {
+    try {
+        const { fullName, email } = req.body;
+
+        if (!fullName || !email) {
+            throw new ApiError(400, "All fields are required");
+        }
+
+        /**
+         * Here, below we have used findByIdAndUpdate and for changeCurrentPassword controller we have just added newPassword directly in user object and then called user.save because of one filed (password) only and we need to call the hook (encryption method (in user.model.js it is defined)) before saving user (when user.save is called)
+         */
+        const user = await User.findByIdAndUpdate(
+            req.user?._id,
+            {
+                $set: {
+                    fullName,
+                    email
+                }
+            },
+            { new: true }
+        ).select("-password");
+
+        return res.status(200).json(
+            new ApiResponse(200, user, "Account details updated successfully")
+        );
+    } catch (error) {
+        throw new ApiError(401, "Unauthorized access");
+    }
+});
+
+const updateUserAvatar = asyncHandler(async (req, res) => {
+    const avatarLocalPath = req.file?.path;
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is missing");
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+    if (!avatar.url) {
+        throw new ApiError(400, "Error while uploading on avatar");
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                avatar: avatar.url
+            }
+        },
+        { new: true }
+    ).select("-password");
+
+    return res.status(200).json(
+        new ApiResponse(200, user, "Avatar updated successfully")
+    );
+});
+
+const updateUserCoverImage = asyncHandler(async (req, res) => {
+    const coverImageLocalPath = req.file?.path;
+
+    if (!coverImageLocalPath) {
+        throw new ApiError(400, "Cover Image file is missing");
+    }
+
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+    if (!coverImage.url) {
+        throw new ApiError(400, "Error while uploading on Cover Image");
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                coverImage: coverImage.url
+            }
+        },
+        { new: true }
+    ).select("-password");
+
+    return res.status(200).json(
+        new ApiResponse(200, user, "Cover Image updated successfully")
+    );
+});
+
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+    const { username } = req.params;
+
+    if (!username?.trim()) {
+        throw new ApiError(400, "username is missing");
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscribersCount: {
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: { $in: [req.user?._id, "$subscriber.subscriber"] },
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                fullName: 1,
+                username: 1,
+                subscribersCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
+            }
+        }
+    ]);
+
+    if (!channel?.length) {
+        throw new ApiError(404, "channel doesn't exists");
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, channel[0], "User channel fetched successfully")
+    );
+});
+
+const getWatchHistory = asyncHandler(async (req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [ // check the below $project pipeline by putting outside of this lookup: "videos"
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    // { // outside here check
+                    //     $project: {
+                    //         fullName: 1,
+                    //         username: 1,
+                    //         subscribersCount: 1,
+                    //         channelsSubscribedToCount: 1,
+                    //         isSubscribed: 1,
+                    //         avatar: 1,
+                    //         coverImage: 1,
+                    //         email: 1
+                    //     }
+                    // }
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ]);
+
+    return res.status(200).json(
+        new ApiResponse(200, user[0].watchHistory, "Watch history fetched successfully")
+    );
+});
+
+export {
+    changeCurrentPassword,
+    getCurrentUser, getUserChannelProfile,
+    getWatchHistory, loginUser,
+    logoutUser,
+    refreshAccessToken,
+    registerUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage
+};
 
